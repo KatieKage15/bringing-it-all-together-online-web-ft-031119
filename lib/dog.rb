8d806b55ddd5bc0d582b34ee94dbba17de438382
victorious-messaging-4309 <wildayk@gmail.com>
2019-04-15 10:28:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -62,6 +62,6 @@ class Dog
       DB[:conn].execute(sql, self.name, self.breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     end
-    self 
+    self
   end
 end
