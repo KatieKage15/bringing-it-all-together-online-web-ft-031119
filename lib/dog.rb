cd762e31634f1c808a37d56ec2d087d1cd42581e
victorious-messaging-4309 <wildayk@gmail.com>
2019-04-15 10:22:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -31,9 +31,24 @@ class Dog
     self.new(id, name, breed)
   end
 
+  def find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
   def update
     sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 
+  def save
+  
 end
